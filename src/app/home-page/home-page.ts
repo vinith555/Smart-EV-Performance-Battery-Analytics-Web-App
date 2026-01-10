517ab00a198a18371269cfde09572805457609c5
unknown <vinithmuthusamy555@gmail.com>
2026-01-10 14:25:59 +0530

    added the home page

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
+  vehicles:{img:string,name:string,des:string}[] = [
+    {img:'/activa-e-right-side-view-9.avif',name:'MG ZS EV',des:'A stylish electric SUV with advanced technology.'},
+    {img:'/windsor-ev-exterior-right-rear-three-quarter-3.avif',name:'Hyundai Kona Electric',des:'A versatile electric vehicle with great performance.'},  
+  ];
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
