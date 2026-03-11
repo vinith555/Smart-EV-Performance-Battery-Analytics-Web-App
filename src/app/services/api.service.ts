76f0861463bde8c3ebc3360b3755988f4384c1b2
tharunkumar-BIT <s.tharunkumar8905@gmail.com>
2026-03-11 15:57:40 +0530

    static files url fix

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://smart-ev-performance-battery-analytics.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
